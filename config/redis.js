f5f5505b0a0abd00109390d3cb20b3a7f86bee7b
Jesse Ditson <jesse.ditson@gmail.com>
2015-09-16 20:21:56 -0700

    work on redis config

--- config/redis.js
+++ config/redis.js
@@ -3,7 +3,13 @@ require('dotenv').load()
 var config
 
 if (process.env.REDIS_URL) {
-  config = require('redis-url').parse(process.env.REDIS_URL)
+  var parsed = require('redis-url').parse(process.env.REDIS_URL)
+  config = {
+    host: parsed.host,
+    port: parsed.port,
+    pass: parsed.password,
+    db: parsed.database
+  }
 } else {
   config = {
     host: process.env.REDIS_HOST || 'localhost',
