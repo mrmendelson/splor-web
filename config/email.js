be8c7a718828208157aadf84c28cc237254d4b72
Jesse Ditson <jesse.ditson@gmail.com>
2015-10-25 00:47:12 -0700

    switch to mailgun for more reliable email delivery

--- config/email.js
+++ config/email.js
@@ -1,13 +1,9 @@
 require('dotenv').load()
 
-if (!process.env.GMAIL_USER) throw new Error('Missing required environment variable GMAIL_USER')
-if (!process.env.GMAIL_PASSWORD) throw new Error('Missing required environment variable GMAIL_PASSWORD')
-
 module.exports = {
-    service: 'Gmail',
-    auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASSWORD
-    },
-    overrideEmail: process.env.OVERRIDE_EMAIL
+  auth: {
+      api_key: process.env.MAILGUN_API_KEY,
+      domain: process.env.MAILGUN_DOMAIN
+  },
+  overrideEmail: process.env.OVERRIDE_EMAIL
 }
